ad2b7113df2005cfa9784aaed8d12f53d37fbf16
szf <szf@singzy.com>
2024-11-13 13:45:36 +0800

    1

--- src/main/java/com/clif/jenkins_demo/JenkinsDemoApplication.java
+++ src/main/java/com/clif/jenkins_demo/JenkinsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsDemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
         SpringApplication.run(JenkinsDemoApplication.class, args);
     }
 
